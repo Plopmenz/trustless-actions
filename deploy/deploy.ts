6dae5c0275e6d25085943ab9093d49af1bc1b883
Plopmenz <plopmenz@gmail.com>
2024-01-29 17:23:51 +0100

    deployment

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,31 +1,20 @@
 import { Address, Deployer } from "../web3webdeploy/types";
-import { deployCounter } from "./counters/Counter";
-import { deployProxyCounter } from "./counters/ProxyCounter";
 
-export interface DeploymentSettings {
-  startingNumber: bigint;
-}
+export interface DeploymentSettings {}
 
 export interface Deployment {
-  counter: Address;
-  proxyCounter: Address;
+  optimisticActions: Address;
 }
 
 export async function deploy(
   deployer: Deployer,
   settings?: DeploymentSettings
 ): Promise<Deployment> {
-  const counter = await deployCounter(deployer);
-  const proxyCounter = await deployProxyCounter(deployer, counter);
-  await deployer.execute({
-    id: "InitialCounterNumber",
-    abi: "Counter",
-    to: counter,
-    function: "setNumber",
-    args: [settings?.startingNumber ?? BigInt(3)],
+  const optimisticActions = await deployer.deploy({
+    id: "OptimisticActions",
+    contract: "OptimisticActions",
   });
   return {
-    counter: counter,
-    proxyCounter: proxyCounter,
+    optimisticActions: optimisticActions,
   };
 }
